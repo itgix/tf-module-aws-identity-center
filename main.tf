53713888dabf50319d09fb6a8ee6aed84d010b96
Boris Yakimov <boris.yakimov@itgix.com>
2024-07-24 16:45:32 +0300

    #50 add permission sets

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 data "aws_ssoadmin_instances" "identity_store" {}
 
+# User
 resource "aws_identitystore_user" "user" {
   for_each          = { for u in var.users : u.user_name => u }
   identity_store_id = tolist(data.aws_ssoadmin_instances.identity_store.identity_store_ids)[0]
@@ -18,6 +19,7 @@ resource "aws_identitystore_user" "user" {
   }
 }
 
+# Group
 resource "aws_identitystore_group" "group" {
   for_each          = { for g in var.groups : g.display_name => g }
   display_name      = each.value.display_name
@@ -39,6 +41,8 @@ locals {
   user_group_pairs = { for ug in local.user_group_list : "${ug.user_name}-${ug.group_name}" => ug }
 }
 
+
+# Assign user to group
 resource "aws_identitystore_group_membership" "group_association" {
   for_each = local.user_group_pairs
 
@@ -47,3 +51,30 @@ resource "aws_identitystore_group_membership" "group_association" {
   member_id         = local.user_ids[each.value.user_name]
 }
 
+
+// TODO: to be tested after Identity Center is recreated with local users
+# Premission set
+resource "aws_ssoadmin_permission_set" "readonly_non_prod" {
+  for_each         = { for p in var.permission_sets : p.name => p }
+  name             = each.value.name
+  description      = each.value.description
+  instance_arn     = tolist(data.aws_ssoadmin_instances.identity_store.arns)[0]
+  relay_state      = "https://s3.console.aws.amazon.com/s3/home?region=${var.aws_region}#"
+  session_duration = each.value.session_duration
+}
+
+#resource "aws_ssoadmin_permission_set" "readonly_non_prod" {
+#name             = "ReadOnlyAccess"
+#description      = "A permission set with read-only access to everything except PROD"
+#instance_arn     = tolist(data.aws_ssoadmin_instances.identity_store.arns)[0]
+#relay_state      = "https://s3.console.aws.amazon.com/s3/home?region=${var.aws_region}#"
+#session_duration = "PT2H"
+#}
+
+#resource "aws_ssoadmin_permission_set" "readonly_prod" {
+#name             = "Prod-ReadOnlyAccess"
+#description      = "A permission set with read-only access to PROD"
+#instance_arn     = tolist(data.aws_ssoadmin_instances.identity_store.arns)[0]
+#relay_state      = "https://s3.console.aws.amazon.com/s3/home?region=${var.aws_region}#"
+#session_duration = "PT1H"
+#}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "AWS Region where Identity Center is created"
+  type        = string
+}
 variable "identity_store_id" {
   description = "The ID of the AWS Identity Store"
   type        = string
@@ -18,9 +22,18 @@ variable "users" {
 }
 
 variable "groups" {
-  description = "List of users that should be created in the Identity Center"
+  description = "List of groups that should be created in the Identity Center"
   type = list(object({
     display_name = string
     description  = string
+  }))
+}
+
+variable "permission_sets" {
+  description = "List of permission sets that should be created in the Identity Center"
+  type = list(object({
+    name             = string
+    description      = string
+    session_duration = string
   }))
 }
